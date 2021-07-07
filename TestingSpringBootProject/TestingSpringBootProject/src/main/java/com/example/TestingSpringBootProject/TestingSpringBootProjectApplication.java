f1d2879a5fb87983a65b560699016e2782701dfd
SHRADDHA <SHRADDHA@DESKTOP-RQKC037>
2021-07-07 20:48:17 +0530

    Commited

--- TestingSpringBootProject/TestingSpringBootProject/src/main/java/com/example/TestingSpringBootProject/TestingSpringBootProjectApplication.java
+++ TestingSpringBootProject/TestingSpringBootProject/src/main/java/com/example/TestingSpringBootProject/TestingSpringBootProjectApplication.java
@@ -10,6 +10,7 @@ public class TestingSpringBootProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestingSpringBootProjectApplication.class, args);
 		System.out.println("Hello");
+		System.out.println("Ji");
 	}
 
 }
