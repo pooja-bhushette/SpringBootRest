e8a9c93d57ec6aeeca683f7814f686446a230629
pooja-bhushette <85172126+pooja-bhushette@users.noreply.github.com>
2021-07-07 19:27:34 +0530

    Add files via upload
    First Commit

---
+++ TestingSpringBootProject/TestingSpringBootProject/src/test/java/com/example/TestingSpringBootProject/TestingSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestingSpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
